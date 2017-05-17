1903ebba4d5f7c2df595abe2e67920a2d6652c7e
qwl <qwl@ncmem.com>
2017-05-17 15:13:06 +0800

    分离FileUploader逻辑

--- WordPaster/js/WordPaster.js
+++ WordPaster/js/WordPaster.js
@@ -27,7 +27,7 @@ var WordPasterConfig = {
 	"EncodeType"		    : "GB2312"
 	, "Company"			    : "荆门泽优软件有限公司"
 	, "Version"			    : "1,5,123,31671"
-	, "License"			    : "DAFB3AE12494FA2E2891238FC22E46AFA6437200A018115574E842DDC2B54DC78558FE1C4701C0504CE530"
+	, "License"			    : ""
 	, "Debug"			    : false//调试模式
 	, "LogFile"			    : "f:\\log.txt"//日志文件路径
 	, "PasteWordType"	    : ""	//粘贴WORD的图片格式。JPG/PNG/GIF/BMP，推荐使用JPG格式，防止出现大图片。
@@ -941,7 +941,18 @@ function WordPasterManager()
 	this.load_complete_edge = function (json)
 	{
 	    _this.event.emit("load_complete");
-	};
+    };
+    this.load_complete = function (json)
+    {
+        var needUpdate = true;
+        if (typeof (json.version) != "undefined") {
+            if (json.version == this.Config.Version) {
+                needUpdate = false;
+            }
+        }
+        if (needUpdate) this.setupTip();
+        else { this.setupTipClose(); }
+    };
 	this.WordParser_StateChanged = function (msg)
 	{
 	    var json = JSON.parse(msg);
@@ -957,130 +968,8 @@ function WordPasterManager()
 	    else if (json.name == "File_PostProcess") _this.File_PostProcess(json);
 	    else if (json.name == "File_PostComplete") _this.File_PostComplete(json);
 	    else if (json.name == "File_PostError") _this.File_PostError(json);
+        else if (json.name == "load_complete") _this.load_complete(json);
 	    else if (json.name == "Queue_Complete") _this.Queue_Complete(json);
 	    else if (json.name == "load_complete_edge") _this.load_complete_edge(json);
-	};
-}
-
-var FileUploaderState = {
-	Ready: 0,
-	Posting: 1,
-	Stop: 2,
-	Error: 3,
-	GetNewID: 4,
-	Complete: 5,
-	WaitContinueUpload: 6,
-	None: 7,
-	Waiting: 8
-};
-
-/*
-	文件上传对象
-	参数：
-		fileID 文件唯一标识
-		filePath 包含完整路径的本地文件名称。D:\Soft\QQ.exe
-		width 图片宽度
-		height 图片高度
-	属性：
-		pMsg		显示上传信息，进度信息
-		pProcess	进度条
-		pPercent	显示百分比文字
-		pButton		按钮按钮
-		LocalFile	本地文件路径。D:\Soft\QQ.exe
-		ImageTag	图片标记。
-		InfDiv		上传信息层。
-		Separator	分隔线
-*/
-function FileUploader(fileID,filePath,mgr,width,height)
-{
-	var _this = this;
-	this.Manager = mgr;
-	this.Editor = mgr.Editor;
-	this.Config = mgr.Config;
-	this.ActiveX = mgr.ActiveX;
-	this.Fields = mgr.Fields;
-	this.Browser = mgr.Browser;
-	this.InsertHtml = mgr.InsertHtml;
-
-	this.PostLocalFile = false;//是否上传本地文件
-	this.imgW = width;
-	this.imgH = height;
-	this.LocaFile = filePath;//网络图片需要使用
-	this.State = FileUploaderState.None;
-	this.LocalFile = filePath;
-	this.FileID = fileID;
-	this.idLoc = fileID;//add(2015-12-10)
-	this.ImageTag = ""; //图片标记，在图片上传完后需要替换此标记
-
-	this.postComplete = function (json)
-	{
-	    this.pButton.text("");
-	    this.pProcess.css("width", "100%");
-	    this.pPercent.text("100%");
-	    this.pMsg.text("上传完成");
-
-	    //从上传列表中清除
-	    this.Remove();
-	    //更新编辑器中的图片标签
-	    this.FilePostComplete(json.pathSvr);
-	};
-
-	this.postError = function (json)
-	{
-	    this.pMsg.text(WordPasterError[json.value]);
-	    this.pButton.text("重试");
-	};
-
-	this.postProcess = function (json)
-	{
-	    var msg = "已上传:" + json.len + " 速度:" + json.speed + " 剩余时间:" + json.time;
-	    this.pMsg.text(msg);
-	    this.pPercent.text(json.percent);
-	    this.pProcess.css("width", json.percent);
-	};
-
-	//方法-准备
-	this.Ready = function()
-	{
-		//this.pButton.style.display = "none";
-		_this.pMsg.text("正在上传队列中等待...");
-		_this.State = FileUploaderState.Ready;
-	};
-
-	//从上传列表中删除
-	this.Remove = function()
-	{
-		//删除信息层
-		_this.InfDiv.remove();
-		//删除分隔线
-		_this.Separator.remove();
-		//清空本地文件名称
-		_this.LocalFile = "";
-	};
-
-	//停止传输
-	this.Stop = function()
-	{
-		//var obj = _this.Manager.UploaderList[fid];
-		this.ATL.Stop();
-		this.State = FileUploaderState.Stop;
-		this.pButton.text("重试");
-	};
-
-	//本地图片文件上传完毕
-	this.LocalFileComplete = function (imgSrc)
-	{
-		var img = '<img src="' + imgSrc + '"/>';
-		_this.InsertHtml(img);
-	};
-
-	//文件上传完毕
-	this.FilePostComplete = function(imgSrc)
-	{
-		//上传的本地文件
-		if (_this.PostLocalFile)
-		{
-			_this.LocalFileComplete(imgSrc);
-		}
 	};
 }
